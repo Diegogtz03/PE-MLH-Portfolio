f33ca3f7b2f9505c8e1bd16ba6ce425c5d34548e
YeyoM <yeyomoreno2003@hotmail.com>
2023-06-22 16:09:17 -0600

    feat: :sparkles: Project Structure and Styling Page

---
+++ app/static/scripts/projects.js
@@ -0,0 +1,47 @@
+const openModal1 = document.getElementById('open-modal-1');
+const modal1 = document.getElementById('modal-1');
+const closeModal1 = document.getElementById('close-modal-1');
+
+openModal1.addEventListener('click', () => {
+  modal1.showModal();
+})
+
+closeModal1.addEventListener('click', () => {
+  modal1.close();
+})
+
+const openModal2 = document.getElementById('open-modal-2');
+const modal2 = document.getElementById('modal-2');
+const closeModal2 = document.getElementById('close-modal-2');
+
+openModal2.addEventListener('click', () => {
+  modal2.showModal();
+})
+
+closeModal2.addEventListener('click', () => {
+  modal2.close();
+})
+
+const openModal3 = document.getElementById('open-modal-3');
+const modal3 = document.getElementById('modal-3');
+const closeModal3 = document.getElementById('close-modal-3');
+
+openModal3.addEventListener('click', () => {
+  modal3.showModal();
+})
+
+closeModal3.addEventListener('click', () => {
+  modal3.close();
+})
+
+const openModal4 = document.getElementById('open-modal-4');
+const modal4 = document.getElementById('modal-4');
+const closeModal4 = document.getElementById('close-modal-4');
+
+openModal4.addEventListener('click', () => {
+  modal4.showModal();
+})
+
+closeModal4.addEventListener('click', () => {
+  modal4.close();
+})
